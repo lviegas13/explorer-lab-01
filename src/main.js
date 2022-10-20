41ea90e5cbbf62b877ad2eb291f78417cd27bf75
Luciano Viegas <lucianoviegasvargas28@gmail.com>
2022-10-20 02:13:34 -0300

    sim

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
 import "./css/index.css"
 import IMask from "imask"
 
-const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
-const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccBgColor1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
 function setCardType(type) {
   const colors = {
-    visa: ["436d99", "#2d57f2"],
-    mastercard: ["#df6f29", "#c69347"],
-    default: ["black", "gray"]
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+
+    //white, yellow ===== "#436d99", "#2d57f2"
+    //violet, yellowgreen ====== "#df6f29", "#c69347"
   }
 
-  ccBgColor01.setAttribute("fill", colors[type][0])
-  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccBgColor1.setAttribute("fill", colors[type][0])
+  ccBgColor2.setAttribute("fill", colors[type][1])
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
 globalThis.setCardType = setCardType
+//setCardType("visa")
 
 const securityCode = document.querySelector("#security-code ")
 const securityCodePattern = {
@@ -69,4 +73,41 @@ const cardNumberPattern = {
     return foundMask
   }
 }
-const cardNumbermasked = IMask(cardNumber, cardNumberPattern)
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  alert("Cartão adiconado.")
+})
+
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+  ccSecurity.innerText = code.length === 0 ? "123" : code
+}
+
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+  ccNumber.innerText = number.length === 0 ? "1234 5678 9012 3456" : number
+}
